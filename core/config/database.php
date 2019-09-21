1b606d3b2840ebd5169aaec6aed6912e423452d6
cambridgejames <cambridge_james@foxmail.com>
2019-09-22 01:29:41 +0800

    修改了数据库连接，将数据库服务从第三方服务器改为当前服务器

--- core/config/database.php
+++ core/config/database.php
@@ -2,9 +2,9 @@
 return array(
 	'database_type' => 'mysql',
 	'database_name' => 'quickbi',
-	'server' => '32bjnzlt.2386.dnstoo.com',
-	'username' => 'quickbi_f',
-	'password' => 'MysqlQuickbif123',
+	'server' => 'localhost',
+	'username' => 'quickbi-user',
+	'password' => 'quickbi',
 	'charset' => 'utf8',
-	'port' => 5503
+	'port' => 3306
 );
